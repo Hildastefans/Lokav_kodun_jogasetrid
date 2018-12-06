14cfa87c6998275ee886e69580c6bf06d541da2b
Hilda Stefánsdóttir <hildabjorg@gmail.com>
2018-12-06 17:56:27 +0000

    CSS, overlay, timatafla, a href og fl.
    CSS, overlay, timatafla, a href og fl.

--- inc/footer.js
+++ inc/footer.js
@@ -1,19 +1,24 @@
 var footerElement = document.querySelector("footer")
 
 footerElement.innerHTML = `
-<div class="container_footer">      
+<div class="container_footer"> 
+  <a href="#" alt="heimilisfang">     
   <div class= "address">
       <h3>Jógasetrið</h3>
       <p>Skipholti 50c <br> 105 Reykjavík <br> Sími: 778 - 1000</p>
   </div>
-
+  </a>
+  <a href="#" alt="Netfang">
   <div class= "logo_email" >
       <img src="images/jogasetrid-hvitt.svg" class="jogasetur_logo" alt="Jógasetrið logo">
       <h3>jogasetrid@jogasetrid.is</h3>
   </div>
-
+  </a>
+  <a href="#" alt="facebook">
   <div class= "social_media" >
       <img src= "images/fb.svg" class="fb" alt="facebook_logo">
   </div>
+  </a>
 </div>
+
 `
